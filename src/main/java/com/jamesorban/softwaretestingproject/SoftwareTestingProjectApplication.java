014fa9884382843ab110556358a17a9f09630033
orbanjames <96356809+orbanjames@users.noreply.github.com>
2022-05-24 00:13:31 +0200

    first commit

---
+++ src/main/java/com/jamesorban/softwaretestingproject/SoftwareTestingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.jamesorban.softwaretestingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareTestingProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftwareTestingProjectApplication.class, args);
+    }
+
+}
